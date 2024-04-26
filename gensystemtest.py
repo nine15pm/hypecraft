eaf12d6a2a8835283f3e68fa984e769058a4ebb4
Project Maintainer <maintainer@example.com>
2024-04-25 20:20:07 -0700

    Updated summary to 2-step summarizer + editor, added initial substack test

--- gensystemtest.py
+++ gensystemtest.py
@@ -5,61 +5,97 @@ import json
 import trafilatura
 import mimetypes
 
-#Configs
+#CONFIGS
+##############################################################################################
+#General
+PATH_SUMMARIES_REDDIT = 'summaries_reddit.json'
+PATH_SUMMARIES_SUBSTACK = 'summaries_substack.json'
+
+#SUMMARIZER_TITLE_PREPEND = '''Use your best effort to confidently write a hyped clickbait headline for the following content, in 15 words or less:\n\n'''
+
+#Summarizer
 SUMMARIZER_MODEL = 'llama3'
-EMAIL_GEN_MODEL = 'llama3'
-
-SUMMARIZER_SYSTEM_PROMPT = '''You are the 25 year old author of a popular daily email newsletter.
-The newsletter covers many topics, like AI, sports, culture, and fitness.
-Your job is to take trending content from blogs, Twitter, Reddit, and other sources and summarize it.
-Your writing style is conversational, direct, and engaging like talking to an interesting friend. You often use jokes and sarcasm in your writing to be more entertaining.
-Your summaries should be highly appealing to read for a young audience.'''
-
-HEADLINE_USER_PROMPT_PREPEND = '''Use your best effort to confidently write a hyped clickbait headline for the following content in 15 words or less:\n\n'''
-SUMMARY_USER_PROMPT_PREPEND = '''Now, use your best effort to confidently write an engaging summary in 200 words or less.'''
-
+SUMMARIZER_SYSTEM_PROMPT = '''Your task is to write a summary of a trending piece of content from a blog, Twitter, Reddit, or other sources. 
+Write in a style that is conversational, direct, and engaging, like chatting with a good friend. 
+Use jokes and sarcasm in the summary to be more entertaining. 
+The summary should be highly appealing to read for a young audience. '''
+SUMMARIZER_PREPEND = '''Use your best effort to provide a summary of the following content, in 200 words or less. Do NOT write in first person.\n\n'''
 SUMMARIZER_MODEL_PARAMS = {
     'mirostat': 2,
-    'temperature': 0.8
+    'temperature': 0.6,
 }
 
-#Function to call model and return json of summary {headline:, summary:}
+#Editor
+EDITOR_MODEL = SUMMARIZER_MODEL
+EDITOR_SYSTEM_PROMPT = '''You are an editor of newsletter content. 
+Your task is to edit the summaries of stories to ensure consistent formatting, ensure consistent tone, and improve clarity.  
+For example, to ensure good formatting you should remove any unnecessary quotation marks, brackets, or markup that should not be shown to the reader. 
+For example, to ensure consistent tone you should reword anything the author wrote in first person (e.g. "we" or "our") into third person. 
+For example, to improve clarity, you may choose to condense or edit sentences that have redundant information. '''
+EDITOR_PREPEND = '''Make edits to the following summary and provide the ONLY text of the improved version. Do NOT respond with chat or comments.\n\n'''
+EDITOR_MODEL_PARAMS = {
+    'mirostat': 2,
+    'temperature': 0.6
+}
+
+#FUNCTIONS
+##############################################################################################
+#Use model to summarize raw content text
 def summarizeContent(content_text):
-    title = o.generate(
+    output = o.generate(
         model = SUMMARIZER_MODEL,
-        prompt = HEADLINE_USER_PROMPT_PREPEND + content_text,
+        prompt = SUMMARIZER_PREPEND + content_text,
         system = SUMMARIZER_SYSTEM_PROMPT,
         options = SUMMARIZER_MODEL_PARAMS,
         stream = False
     )
+    print(output['prompt_eval_count'])
+    return output['response']
 
-    body = o.generate(
-        model = SUMMARIZER_MODEL,
-        prompt = SUMMARY_USER_PROMPT_PREPEND,
-        system = SUMMARIZER_SYSTEM_PROMPT,
-        context = title['context'], #prior conversation context
-        options = SUMMARIZER_MODEL_PARAMS,
+#Use model to edit output summaries to ensure consistent formatting, tone, quality
+def editSummary(summary_text):
+    output = o.generate(
+        model = EDITOR_MODEL,
+        prompt = EDITOR_PREPEND + summary_text,
+        system = EDITOR_SYSTEM_PROMPT,
+        options = EDITOR_MODEL_PARAMS,
         stream = False
     )
+    return output['response']
 
-    gen_time_body = float(body['total_duration'])/1e9
-    print(f"{gen_time_body}s")
 
-    full_summary = {
-        'headline': title['response'],
-        'summary': body['response']
-    }
-    return full_summary
+def editSummary(summary_text):
+    output = o.generate(
+        model = EDITOR_MODEL,
+        prompt = EDITOR_PREPEND + summary_text,
+        system = EDITOR_SYSTEM_PROMPT,
+        options = EDITOR_MODEL_PARAMS,
+        stream = False
+    )
+    return output['response']
 
-#Generation test using a a test substack and subreddit
+#Save to local json file
+def saveJSON(data, path):
+    with open(path, 'w') as outfile:
+        json.dump(data, outfile)
+
+#Read from local json file
+def loadJSON(path):
+    with open(path, 'r') as infile:
+        return json.load(infile)
+
+#GENERATION TEST
+##############################################################################################
 subreddit = 'formula1'
 substack = 'semianalysis'
-last7days = time.time() - 604800 #get current time minus 7 days
+last30days = time.time() - 2.6e6 #get current time minus 30 days
 
-subreddit_posts = getRedditPosts(subreddit, max_posts=2, newer_than_datetime=last7days)
-substack_posts = getSubstackPosts(substack, newer_than_datetime=last7days)
+#Get posts from sources
+subreddit_posts = getRedditPosts(subreddit, max_posts=2, newer_than_datetime=last30days)
+substack_posts = getSubstackPosts(substack, newer_than_datetime=last30days)
 
-subreddit_summaries = []
+#Generate and save summaries of individual posts
+reddit_summaries = []
 substack_summaries = []
 
 for post in subreddit_posts:
@@ -79,12 +115,29 @@ for post in subreddit_posts:
             linked_text = ""
         
         content = post_title + "/n" + post_text + "/n/n" + linked_text
-        print(summarizeContent(content)['headline'])
-        print(summarizeContent(content)['summary'])
-        #subreddit_summaries.append()
+        original = summarizeContent(content)
+        edited = editSummary(original)
+        print('EDITED----------------')
+        print(edited)
+        reddit_summaries.append(edited)
 
-#for s in subreddit_summaries:
-    #print(s['headline'])
-    #print("/n")
-    #print(s['summary'])
-    #print("/n/n")
+for post in substack_posts:
+    post_title = post['headline']
+    post_description = post['description'] if post['description'] is not None else ""
+    post_content = post['post_content']
+    content = post_title + "/n" + post_description + "/n/n" + post_content
+    original = summarizeContent(content)
+    print('ORIGINAL----------------')
+    print(original)
+    edited = editSummary(original)
+    print('EDITED----------------')
+    print(edited)
+    substack_summaries.append(edited)
+
+#save summaries to local file
+saveJSON(reddit_summaries, PATH_SUMMARIES_REDDIT)
+saveJSON(substack_summaries, PATH_SUMMARIES_SUBSTACK)
+
+#Assemble newsletter
+
+
