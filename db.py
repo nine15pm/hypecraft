88a35d5e76d6a9ba8913560c444eae2b3e992bbd
Project Maintainer <maintainer@example.com>
2024-05-16 15:06:32 -0700

    added delete posts function

--- db.py
+++ db.py
@@ -355,3 +355,8 @@ def deleteStories(min_datetime=MIN_DATETIME_DEFAULT, max_datetime=MAX_DATETIME_D
     table = STORY_TABLE
     deleteEntries(table=table, min_datetime=min_datetime, filters=filters, max_datetime=max_datetime)
     print(f'Stories from {min_datetime} to {max_datetime} deleted')
+
+def deletePosts(min_datetime=MIN_DATETIME_DEFAULT, max_datetime=MAX_DATETIME_DEFAULT, filters={}):
+    table = POST_TABLE
+    deleteEntries(table=table, min_datetime=min_datetime, filters=filters, max_datetime=max_datetime)
+    print(f'Posts from {min_datetime} to {max_datetime} deleted')
