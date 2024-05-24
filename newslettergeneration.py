3bc39b791c00f22cba22415046f714cab2f6b1f9
Project Maintainer <maintainer@example.com>
2024-05-24 16:21:44 -0700

    added list of top RAG search results for QA

--- newslettergeneration.py
+++ newslettergeneration.py
@@ -3,6 +3,7 @@ import configs
 import utils
 import emailer
 import changelog
+import RAG
 from pytz import timezone
 from datetime import datetime, time, timedelta
 
@@ -18,9 +19,9 @@ def recordUsage(stories:list[dict], newsletter_date):
             'used_in_newsletter': True,
             'newsletter_date': newsletter_date
         })
-        for post in story['posts_summarized']:
+        for post_id in story['posts_summarized']:
             post_updates.append({
-                'post_id': post['post_id'],
+                'post_id': post_id,
                 'used_in_newsletter': True,
                 'newsletter_date': newsletter_date
             })
@@ -36,6 +37,14 @@ def storyUnit(tag:str, headline:str, body:str, links:list):
     links_html = ''
     for i, link in enumerate(links):
         links_html = links_html + ampLink(text=f'Link {i}', url=link)
+    
+    #Show RAG results for QA
+    results = RAG.searchStories(text=f'{headline}/n/n{body}', max_results=4)
+    rag_string = ''
+    for item in results:
+        story = db.getStories(filters={'story_id':item['id']})[0]
+        rag_string = rag_string + f'- [Cos-sim: {item['sim_score']}] [Past newsletter: {story['used_in_newsletter']}] {story['headline_ml']}\n'
+
     output_html = f'''
         <div class="unit-story">
             <span class="tag">{tag}</span>
@@ -43,6 +52,11 @@ def storyUnit(tag:str, headline:str, body:str, links:list):
             <p class="paragraph">{body}</p>
             <div class="unit-story-links">
                 {links_html}
+            </div>
+            <div>
+                <p class="small-text">
+                    {utils.linebreaksHTML(rag_string)}
+                </p>
             </div>
         </div>
         '''
@@ -70,7 +84,7 @@ def ampAccordion(units:list[tuple]):
 #BLOCK CONSTRUCTORS
 ##############################################################################################
 #Prep a news themes/stories block within a topic section
-def constructNewsBlock(topic_id, top_k_stories, min_datetime):
+def constructNewsBlock(topic_id, top_k_stories, min_datetime, newsletter_date):
     themes = db.getThemesForTopic(topic_id, min_datetime=min_datetime)
     units = []
     story_candidates = []
@@ -115,7 +129,7 @@ def constructNewsBlock(topic_id, top_k_stories, min_datetime):
     units.append((parent, child))
 
     #record usage of stories and posts in newsletter
-    recordUsage(story_candidates)
+    recordUsage(stories=story_candidates, newsletter_date=newsletter_date)
 
     #add unused stories accordion section if applicable
     if unused_stories != []:
@@ -194,7 +208,7 @@ def constructFooterSection(footer_text):
     return output_html
 
 #Combine several blocks (e.g. topic highlights, news stories) into an overall topic section
-def constructTopicSection(topic_id, min_datetime, top_k_stories, has_highlight=True, has_news=True):
+def constructTopicSection(topic_id, min_datetime, newsletter_date, top_k_stories, has_highlight=True, has_news=True):
     #get topic heading
     topic = db.getTopics(filters={'topic_id': topic_id})[0]
     topic_heading = f'''<h2>{topic['topic_email_name']}</h2>'''
@@ -203,7 +217,7 @@ def constructTopicSection(topic_id, min_datetime, top_k_stories, has_highlight=T
     highlight_block = constructHighlightBlock(topic_id, min_datetime) if has_highlight else ''
     
     #construct news block
-    news_block = constructNewsBlock(topic_id, top_k_stories, min_datetime) if has_news else ''
+    news_block = constructNewsBlock(topic_id, top_k_stories, min_datetime, newsletter_date) if has_news else ''
 
     output_html = f'''
     <div class="section-topic">
@@ -227,10 +241,11 @@ def wrapEncodeHTML(body_html, template_path):
 ##############################################################################################
 PATH_EMAIL_TEMPLATE = 'emailtemplates/amptemplate_v004.html'
 today_start = datetime.combine(datetime.today(), time.min).astimezone(timezone(configs.LOCAL_TZ))
+newsletter_date = datetime.today()
 topics = [{'topic_id': 1}, {'topic_id': 2}]
 title = 'HYPECRAFT V0.0.4 TEST'
 top_k_stories = 5
-footer_text = f'''<br><br><p><small>🫶 Written for you with love by Hypecraft on {datetime.strftime(datetime.now(), "%A, %B %m")}. Powered by Lllama 3.</small></p>'''
+footer_text = f'''🫶 Written for you with love by Hypecraft on {datetime.strftime(newsletter_date, "%A, %B %m")}. Powered by Lllama 3.'''
 recipients1 = ['maintainer@example.com']
 recipients2 = ['maintainer@example.com', 'contributor@example.com']
 
@@ -238,7 +253,7 @@ header = constructTopHeaderSection(title)
 log = constructChangelogSection(changelog.changelog_current)
 main_content = ''
 for topic in topics:
-    main_content = main_content + constructTopicSection(topic_id=topic['topic_id'], top_k_stories=top_k_stories, min_datetime=today_start)
+    main_content = main_content + constructTopicSection(topic_id=topic['topic_id'], top_k_stories=top_k_stories, min_datetime=today_start, newsletter_date=newsletter_date)
 footer = constructFooterSection(footer_text=footer_text)
 newsletter_html = wrapEncodeHTML(body_html=header + log + main_content + footer, template_path=PATH_EMAIL_TEMPLATE)
 #print(newsletter_html)
